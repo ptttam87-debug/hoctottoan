b893f5a95c8be8d477287ab7de98ab0db83dc36b
ptttam87-debug <ptttam87@gmail.com>
2026-01-28 21:02:49 +0700

    up code 6

---
+++ baigiang-bai5/data/slide19.js
@@ -0,0 +1,10 @@
+(function(){var loadHandler=window['sl_{8F76B427-7CC2-4CC2-A3F5-925FCA34EA16}'];loadHandler&&loadHandler(18, '<div id="spr0_2902b3f"><div id="spr1_2902b3f" class="kern slide"><img id="img1_2902b3f" src="data/img0.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_2902b3f" class="kern slide"><div id="svg0_2902b3f" style="left:53.95px;top:45.85px;"><svg width="599" height="449" viewBox="0 0 599 449"><path fill="#f7fd9d" d="M0,0 h598.85 v448.3 h-598.85 Z"/></svg></div><div id="spr3_2902b3f" style="left:62px;top:67px;"><img id="img0_2902b3f" src="data/img41.png" width="560" height="397" alt="Giải:\
+a) Khi bán 𝑥 mét vải lụa:\
+Số tiền thu được là: 𝐵 𝑥 =220𝑥 (nghìn đồng).\
+Lợi nhuận thu được là: \
+𝐿 𝑥 =𝐵 𝑥 −𝐶 𝑥 =− 𝑥 3 +3 𝑥 2 +240𝑥−500 \
+(nghìn đồng).\
+b) Hàm số 𝐿 𝑥  xác định trên  1 ; 1 8 .\
+  𝐿 ′  𝑥 =−3 𝑥 2 +6𝑥+240\
+ 𝐿 ′  𝑥 =0⇔𝑥=10 hoặc 𝑥=−8 (loại).\
+"/></div></div></div>', '{"s":[]}');})();
---
+++ baigiang-bai5/data/slide21.js
@@ -0,0 +1,8 @@
+(function(){var loadHandler=window['sl_{8F76B427-7CC2-4CC2-A3F5-925FCA34EA16}'];loadHandler&&loadHandler(20, '<div id="spr0_2902e2d"><div id="spr1_2902e2d" class="kern slide"><img id="img2_2902e2d" src="data/img0.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_2902e2d" class="kern slide"><div id="spr3_2902e2d" style="left:12px;top:26.95px;"><div id="svg0_2902e2d" style="left:0px;top:0px;"><svg width="691" height="479" viewBox="0 0 691 479"><path fill="#f7fd9d" d="M0,0 h690.95 v478.3 h-690.95 Z"/></svg></div><div id="spr4_2902e2d" style="left:8px;top:16.05px;"><img id="img0_2902e2d" src="data/img43.png" width="673" height="450" alt="   + Đồ thị:\
+\
+\
+\
+\
+\
+c) Quan sát đồ thị hàm số, ta nhận thấy khi 𝑥=10 thì hàm số đạt giá trị lớn nhất là 1200. Như vậy, hộ làm nghề dệt cần sản xuất và bán ra mỗi ngày 10 mét vải lụa để thu được lợi nhuận tối đa. Lợi nhuận tối đa này là 1200 nghìn đồng.\
+"/></div><div id="spr5_2902e2d" style="left:200.35px;top:8.45px;"><img id="img1_2902e2d" src="data/img44.png" width="363" height="277" alt="" style="left:0.2px;"/></div></div></div></div>', '{"s":[]}');})();
